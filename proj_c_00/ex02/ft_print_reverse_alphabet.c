ddf2304da277b2cd7790aecd89feb20c983943a3
marcello-alves <marcelloa.adm@gmail.com>
2024-08-29 11:21:46 -0300

    envio do ex02

---
+++ proj_c_00/ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: malzir-d <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/28 15:20:25 by malzir-d          #+#    #+#             */
+/*   Updated: 2024/08/29 00:40:46 by malzir-d         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_reverse_alphabet(void)
+{
+	char	i;
+
+	i = 'z';
+	while (i >= 'a')
+	{
+		write(1, &i, 1);
+		i--;
+	}
+}
+
+int	main(void)
+{
+	ft_print_reverse_alphabet();
+	return (0);
+}
